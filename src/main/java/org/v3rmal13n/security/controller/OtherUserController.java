4171e55660c5b74e27c672da3d699b8b685d2931
Gal Martirosyan <v3rmal13n@gmail.com>
2023-10-19 17:43:27 +0300

    подбор готов

--- src/main/java/org/v3rmal13n/security/controller/OtherUserController.java
+++ src/main/java/org/v3rmal13n/security/controller/OtherUserController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.v3rmal13n.security.repository.ProfileRepository;
 import org.v3rmal13n.security.user.Profile;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -32,6 +33,36 @@ public class OtherUserController {
 //        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
 //    }
 
+
+
+//    @GetMapping("/profile")
+//    public ResponseEntity<Integer> otherProfile(Authentication authentication) {
+//        String email = authentication.getName();
+//        Profile profile = profileRepository.findByEmail(email);
+//        String gender = profile.getGender();
+//        int age = profile.getAge();
+//        int minAge = age - 3;
+//        int maxAge = age + 3;
+//
+//        if (profile != null && "male".equals(gender)) {
+//            List<Profile> profiles = profileRepository.findByGenderAndAgeBetween(
+//                     "female", minAge, maxAge
+//            );
+//            int count = profiles.size();
+//            Random rand = new Random();
+//            return ResponseEntity.ok(count);
+//        }
+//        if (profile != null && "female".equals(gender)) {
+//            List<Profile> profiles = profileRepository.findByGenderAndAgeBetween(
+//                    "male", minAge, maxAge
+//            );
+//            int count = profiles.size();
+//            Random rand = new Random();
+//            return ResponseEntity.ok(count);
+//        }
+//        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+//    }
+
     @GetMapping("/profile")
     public ResponseEntity<?> otherProfile(Authentication authentication) {
         String email = authentication.getName();
@@ -41,21 +72,24 @@ public class OtherUserController {
         int minAge = age - 3;
         int maxAge = age + 3;
 
-        if (profile != null && "male".equals(gender)) {
-            List<Profile> profiles = profileRepository.findByGenderAndAgeBetween(
-                    "female", minAge, maxAge
-            );
-            int count = profiles.size();
-            return ResponseEntity.ok(count);
-        }
-        if (profile != null && "female".equals(gender)) {
-            List<Profile> profiles = profileRepository.findByGenderAndAgeBetween(
-                    "male", minAge, maxAge
-            );
-            int count = profiles.size();
-            return ResponseEntity.ok(count);
-        }
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-    }
+        if (profile != null) {
+            List<Profile> profiles = new ArrayList<>();
 
+            if ("male".equals(gender)) {
+                profiles = profileRepository.findByGenderAndAgeBetween("female", minAge, maxAge);
+            } else if ("female".equals(gender)) {
+                profiles = profileRepository.findByGenderAndAgeBetween("male", minAge, maxAge);
+            }
+
+            if (!profiles.isEmpty()) {
+                Random rand = new Random();
+                int randIndex = rand.nextInt(profiles.size());
+                Profile randProfile = profiles.get(randIndex);
+
+                return ResponseEntity.ok(randProfile);
+            }
+        }
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+    }
 }
